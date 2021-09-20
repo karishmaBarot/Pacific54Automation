ea03b8c575648ab4f86f7cf52be1e7f7b44ad63b
karishmaBarot <89440968+karishmaBarot@users.noreply.github.com>
2021-09-20 13:35:09 +0530

    Add files via upload

---
+++ TC001_Test.java
@@ -0,0 +1,16 @@
+package testcasespack;
+
+import basepack.BaseClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class TC001_Test extends BaseClass {
+
+
+
+    @Test
+    public void test() throws IOException {
+        setUp();
+    }
+}
